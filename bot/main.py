f8927013b62709d5644249d7a4168a6c1f21f9c9
Alexis Henry <91117127+AlxisHenry@users.noreply.github.com>
2023-10-31 09:29:47 +0100

    Close #1: allow to run the script from root

--- bot/main.py
+++ bot/main.py
@@ -6,9 +6,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+root = False
+
+if "--r" in os.sys.argv:
+    root = True
+
 load_dotenv()
 
-service = Service(os.getenv('CHROMEDRIVER_PATH'))
+chrome_driver_path = os.getenv('CHROMEDRIVER_PATH')
+
+if not root:
+    chrome_driver_path = "./bot/" + chrome_driver_path[2:]
+
+service = Service(chrome_driver_path)
 options = webdriver.ChromeOptions()
 options.add_argument("--user-data-dir=" + os.getenv('CHROME_PROFILE_PATH'))
 options.add_argument("--profile-directory=Default")
@@ -18,10 +28,10 @@ driver.get(os.getenv('WEBSITE_URL'))
 
 t.sleep(2)
 
-lastAutograde = driver.find_element(
+last_autograde = driver.find_element(
     By.XPATH, "/html/body/div/div/main/div[2]/div[1]/div/div[1]/h1").text
-[course, createdAt] = lastAutograde.split("\n")
-[date, time] = createdAt.split(" ")
+[course, created_at] = last_autograde.split("\n")
+[date, time] = created_at.split(" ")
 [day, month, year] = date.split("/")
 [hour, minute] = time.split(":")
 
@@ -40,7 +50,7 @@ data = {
 
 print(data)
 
-with open("./autograder.json", "w") as file:
+with open(root and "./autograder.json" or "./bot/autograder.json", "w") as file:
     json.dump(data, file, indent=4)
 
 driver.quit()
