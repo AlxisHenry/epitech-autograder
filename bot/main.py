00eb940c08588bdd3c15910491517a592638f05c
AlxisHenry <alexis.henry150357@gmail.com>
2023-11-02 00:32:56 +0100

    feat: login fix: remove logs

--- bot/main.py
+++ bot/main.py
@@ -31,6 +31,9 @@ options.add_argument("--user-data-dir=" + os.getenv('PROFILE_PATH'))
 options.add_argument("--profile-directory=Defadult")
 options.add_argument("--headless")
 
+if browser == "edge":
+    options.add_argument('log-level=3')
+
 if browser == "chrome":
     driver = webdriver.Chrome(service=service, options=options)
 else:
@@ -40,29 +43,39 @@ driver.get(os.getenv('WEBSITE_URL'))
 
 t.sleep(2)
 
-last_autograde = driver.find_element(
-    By.XPATH, "/html/body/div/div/main/div[2]/div[1]/div/div[1]/h1").text
-[course, created_at] = last_autograde.split("\n")
-[date, time] = created_at.split(" ")
-[day, month, year] = date.split("/")
-[hour, minute] = time.split(":")
+if driver.find_elements(By.XPATH, "/html/body/div/div/main/div[2]/div[1]/div/div[1]/h1") == []:
+    if driver.find_elements(By.XPATH, "/html/body/div/div/a/span") != []:
+        driver.find_element(By.XPATH, "/html/body/div/div/a/span").click()
+        t.sleep(2)
+    else:
+        driver.quit()
+        exit()
 
-data = {
-    "course": course,
-    "date": {
-        "day": day,
-        "month": month,
-        "year": year
-    },
-    "time": {
-        "hour": hour,
-        "minute": minute
+if driver.find_elements(By.XPATH, "/html/body/div/div/main/div[2]/div[1]/div/div[1]/h1") != []:
+    last_autograde = driver.find_element(
+        By.XPATH, "/html/body/div/div/main/div[2]/div[1]/div/div[1]/h1").text
+    
+    driver.quit()
+
+    [course, created_at] = last_autograde.split("\n")
+    [date, time] = created_at.split(" ")
+    [day, month, year] = date.split("/")
+    [hour, minute] = time.split(":")
+
+    data = {
+        "course": course,
+        "date": {
+            "day": day,
+            "month": month,
+            "year": year
+        },
+        "time": {
+            "hour": hour,
+            "minute": minute
+        }
     }
-}
 
-print(data)
+    print(data)
 
-with open(root and "./autograder.json" or "./bot/autograder.json", "w") as file:
-    json.dump(data, file, indent=4)
-
-driver.quit()
+    with open(root and "./autograder.json" or "./bot/autograder.json", "w") as file:
+        json.dump(data, file, indent=4)
